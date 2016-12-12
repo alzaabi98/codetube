a1ba5ba70e82cd812f794b8f1606dd3c6b4c9006
Abdulziz <alzaabi98@gmail.com>
2016-12-12 09:36:43 +0400

    adding vidoes index 7

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -45,7 +45,8 @@
 
                             @endforeach
 
-                            {{ $video->links()}}
+                            {{ $videos->links() }}
+
 
                         @else
 
